4f3b33189d5cbd5a92ca7b686d2dcec2ea1c238c
alpha-dever <alphadever@gmail.com>
2018-11-16 15:24:03 -0500

    added new problem

---
+++ Problems.Tests/OrphanIntInArrayProblemTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Problems.Tests
+{
+    public class OrphanIntInArrayProblemTest
+    {
+        private readonly OrphanIntInArrayProblem _problem;
+
+        public OrphanIntInArrayProblemTest()
+        {
+            _problem = new OrphanIntInArrayProblem();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int[] input = new int[]{2,2,1};
+
+            var result = _problem.SingleNumber(input);
+            var result2 = _problem.SingleNumber2(input);
+            Assert.Equal(1, result);
+            Assert.Equal(1, result2);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] input = new int[] { 4,1,2,1,2 };
+
+            var result = _problem.SingleNumber(input);
+            var result2 = _problem.SingleNumber2(input);
+            Assert.Equal(4, result);
+            Assert.Equal(4, result2);
+        }
+    }
+}
---
+++ Problems/OrphanIntInArrayProblem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problems
+{
+    /* Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+        Note:
+        Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?*/
+    public class OrphanIntInArrayProblem
+    {
+        Hashtable countTbl = new Hashtable();
+        Stack<int> stack = new Stack<int>();
+        public int SingleNumber(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(countTbl.Contains(nums[i]))
+                {
+                    countTbl[nums[i]] = (int)countTbl[nums[i]] + 1;
+                }
+                else
+                    countTbl.Add(nums[i],0);
+            }
+
+            foreach (DictionaryEntry item in countTbl)
+            {
+                if((int)item.Value == 0)
+                    return (int)item.Key;
+            }
+            return -100000;
+        }
+
+        public int SingleNumber2(int[] nums)
+        {
+            int a = 0;
+            foreach (int num in nums)
+            {
+                a ^= num;
+            }
+            return a;
+        }
+    }
+}
