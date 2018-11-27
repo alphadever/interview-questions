7a8d081087864754a48735b33143184cec24b157
alpha-dever <alphadever@gmail.com>
2018-11-27 16:55:47 -0500

    added a new problem

---
+++ Problems.Tests/JewelsAndStonesProblemTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Problems.Tests
+{
+    public class JewelsAndStonesProblemTest
+    {
+        private readonly JewelsAndStonesProblem _problem;
+        public JewelsAndStonesProblemTest()
+        {
+            _problem = new JewelsAndStonesProblem();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            string J = "aA";
+            string S = "aAAbbbbsadverfrerafevdfvAAAAAAAdwedew";
+
+            var result = _problem.NumJewelsInStones(J,S);
+            Assert.Equal(12, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            string J = "z";
+            string S = "ZZ";
+
+            var result = _problem.NumJewelsInStones(J, S);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            string J = "aA";
+            string S = "aAAbbbbsadverfrerafevdfvAAAAAAAdwedew";
+
+            var result = _problem.NumJewelsInStones2(J, S);
+            Assert.Equal(12, result);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            string J = "z";
+            string S = "ZZ";
+
+            var result = _problem.NumJewelsInStones2(J, S);
+            Assert.Equal(0, result);
+        }
+    }
+}
---
+++ Problems/JewelsAndStonesProblem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems
+{
+    /*
+        You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
+        Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+        The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, 
+        so "a" is considered a different type of stone from "A".
+
+        Example 1:
+
+        Input: J = "aA", S = "aAAbbbb"
+        Output: 3
+        Example 2:
+
+        Input: J = "z", S = "ZZ"
+        Output: 0
+        Note:
+
+        S and J will consist of letters and have length at most 50.
+        The characters in J are distinct.
+     */
+
+    public class JewelsAndStonesProblem
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            byte cnt = 0;
+
+            foreach (char s in S)
+            {
+                foreach (char j in J)
+                {
+                    if(s.Equals(j))
+                    {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            return cnt;
+        }
+
+        public int NumJewelsInStones2(string J, string S)
+        {
+            int cnt = 0;
+            HashSet<Char> jHash = new HashSet<Char>();
+            foreach (char j in J)
+            {
+                jHash.Add(j);
+            }
+            foreach (char s in S)
+            {
+                if(jHash.Contains(s))
+                    cnt++;
+            }
+            return cnt;
+        }
+    }
+}
