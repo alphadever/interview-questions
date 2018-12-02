03c8a2325da5190f18ae45560edcc5b0d607e156
alphadever <alphadever@gmail.com>
2018-12-02 17:12:22 -0500

    added a problem

---
+++ Problems.Tests/RotateArrayProblemTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Problems.Tests
+{
+    public class RotateArrayProblemTest
+    {
+        private readonly RotateArrayProblem _problem;
+
+        public RotateArrayProblemTest()
+        {
+            _problem = new RotateArrayProblem();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int k = 3;
+            int[] input = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            int[] res = new int[] { 5, 6, 7, 1, 2, 3, 4 };
+
+            var result = _problem.Rotate(input, k);
+            Assert.Equal(res, result);
+        }
+    }
+}
---
+++ Problems/RotateArrayProblem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems
+{
+    public class RotateArrayProblem
+    {
+        public int[] Rotate(int[] nums, int k)
+        {
+            int[] ans = new int[nums.Length];
+            for (int i = 1; i < k; i++)
+            {
+                ans[i] = nums[nums.Length - k + i];
+            }
+            for (int i = 0; i < k + 1; i++)
+            {
+                ans[k + i] = nums[i];
+            }
+            return ans;
+        }
+    }
+}
